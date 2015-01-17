29c9ee24873918b2e0fe197e3974a411a21f1303
Alejandro Sedeño <asedeno@zapprx.com>
2015-01-16 21:23:37 -0500

    sha-1 filters

---
+++ sha1filter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from hashlib import sha1
+
+@prefix('SHA-1 hash of lowercased word, expressed in hexadecimal, contains')
+def SHA1Contains(words, x):
+    x = x.strip().lower()
+    return [word for word in words if sha1(word.lower()).hexdigest().find(x) is not -1]
+
+@prefix('SHA-1 hash of lowercased word, expressed in hexadecimal, ends with')
+def SHA1EndsWith(words, x):
+    x = x.strip().lower()
+    return [word for word in words if sha1(word.lower()).hexdigest().endswith(x)]
+
+@prefix('SHA-1 hash of lowercased word, expressed in hexadecimal, starts with')
+def SHA1StartsWith(words, x):
+    x = x.strip().lower()
+    return [word for word in words if sha1(word.lower()).hexdigest().startswith(x)]
